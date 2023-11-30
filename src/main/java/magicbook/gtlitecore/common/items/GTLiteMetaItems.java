27aab75b8650aed211cd5a423ce6e809815bb6fe
Magic_Sweepy <admin>
2023-11-30 18:55:05 +0800

    Add Circuits.

--- src/main/java/magicbook/gtlitecore/common/items/GTLiteMetaItems.java
+++ src/main/java/magicbook/gtlitecore/common/items/GTLiteMetaItems.java
@@ -11,11 +11,65 @@ public class GTLiteMetaItems {
     public static MetaItem<?>.MetaValueItem GOOWARE_BOARD;
     public static MetaItem<?>.MetaValueItem OPTICAL_BOARD;
     public static MetaItem<?>.MetaValueItem SPINTRONIC_BOARD;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_BOARD;
 
     //  Circuit Boards
     public static MetaItem<?>.MetaValueItem GOOWARE_CIRCUIT_BOARD;
     public static MetaItem<?>.MetaValueItem OPTICAL_CIRCUIT_BOARD;
     public static MetaItem<?>.MetaValueItem SPINTRONIC_CIRCUIT_BOARD;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_BOARD;
+
+    //  Circuits
+    public static MetaItem<?>.MetaValueItem GOOWARE_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem GOOWARE_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem GOOWARE_COMPUTER;
+    public static MetaItem<?>.MetaValueItem GOOWARE_MAINFRAME;
+    public static MetaItem<?>.MetaValueItem OPTICAL_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem OPTICAL_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem OPTICAL_COMPUTER;
+    public static MetaItem<?>.MetaValueItem OPTICAL_MAINFRAME;
+    public static MetaItem<?>.MetaValueItem SPINTRONIC_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem SPINTRONIC_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem SPINTRONIC_COMPUTER;
+    public static MetaItem<?>.MetaValueItem SPINTRONIC_MAINFRAME;
+    public static MetaItem<?>.MetaValueItem COSMIC_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem COSMIC_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem COSMIC_COMPUTER;
+    public static MetaItem<?>.MetaValueItem COSMIC_MAINFRAME;
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_PROCESSOR;
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_ASSEMBLY;
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_COMPUTER;
+    public static MetaItem<?>.MetaValueItem SUPRACAUSAL_MAINFRAME;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_ULV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_LV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_MV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_HV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_EV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_IV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_LuV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_ZPM;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_UV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_UHV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_UEV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_UIV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_UXV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_OpV;
+    public static MetaItem<?>.MetaValueItem SUPRACHRONAL_CIRCUIT_MAX;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_ULV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_LV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_MV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_HV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_EV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_IV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_LuV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_ZPM;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_UV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_UHV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_UEV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_UIV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_UXV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_OpV;
+    public static MetaItem<?>.MetaValueItem MAGNETO_RESONATIC_CIRCUIT_MAX;
 
     private GTLiteMetaItems() {}
 
