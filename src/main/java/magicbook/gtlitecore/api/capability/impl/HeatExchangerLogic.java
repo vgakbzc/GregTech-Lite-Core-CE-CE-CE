46b287fe07c9fa4ebb2d71f7f1e31013f0cab046
Magic_Sweepy <15581235-Magic_Sweepy@users.noreply.gitlab.com>
2023-12-16 03:00:28 +0000

    Some multiblocks

---
+++ src/main/java/magicbook/gtlitecore/api/capability/impl/HeatExchangerLogic.java
@@ -0,0 +1,220 @@
+package magicbook.gtlitecore.api.capability.impl;
+
+import gregtech.api.recipes.Recipe;
+import gregtech.api.recipes.RecipeBuilder;
+import gregtech.api.unification.material.Materials;
+import gregtech.api.util.GTUtility;
+import magicbook.gtlitecore.api.capability.GTLiteDataCode;
+import magicbook.gtlitecore.api.metatileentity.multi.IHeatExchanger;
+import magicbook.gtlitecore.api.metatileentity.multi.NoEnergyMultiblockController;
+import magicbook.gtlitecore.api.recipe.properties.FlowRateProperty;
+import magicbook.gtlitecore.api.recipe.properties.MaxRateProperty;
+import magicbook.gtlitecore.api.utils.GTLiteUtils;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fluids.FluidStack;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HeatExchangerLogic extends NoEnergyMultiblockRecipeLogic {
+
+    private int currentHeat;
+    private final int maxHeat;
+    private boolean isSuperheat;
+    private int rate;
+
+    public HeatExchangerLogic(NoEnergyMultiblockController tileEntity) {
+        super(tileEntity, tileEntity.recipeMap);
+        this.maxHeat = ((IHeatExchanger)tileEntity).getHeatTime() * 20;
+    }
+
+    @Override
+    public void update() {
+        if ((!isActive() || !canProgressRecipe() || !isWorkingEnabled()) && currentHeat > 0) {
+            setHeat(currentHeat - 1);
+            setRate(0);
+            setSuperheat(false);
+        }
+        super.update();
+    }
+
+    @Override
+    protected void updateRecipeProgress() {
+        super.updateRecipeProgress();
+        if (!canRecipeProgress) return;
+
+        if (currentHeat < maxHeat) {
+            setHeat(currentHeat + 1);
+        }
+    }
+
+    @Override
+    protected boolean checkPreviousRecipe() {
+        if (this.previousRecipe == null) {
+            return false;
+        } else {
+            List<FluidStack> fluidStackList = new ArrayList<>(GTUtility.fluidHandlerToList(getInputTank()));
+            fluidStackList.add(Materials.DistilledWater.getFluid(Integer.MAX_VALUE));
+            return (long) this.previousRecipe.getEUt() <= this.getMaxVoltage() && this.previousRecipe.matches(false, Collections.emptyList(), fluidStackList);
+        }
+    }
+
+    @Override
+    protected void trySearchNewRecipeCombined() {
+        long maxVoltage = getMaxVoltage();
+        List<FluidStack> fluidStackList = new ArrayList<>(GTUtility.fluidHandlerToList(getInputTank()));
+        fluidStackList.add(Materials.DistilledWater.getFluid(Integer.MAX_VALUE));
+        Recipe currentRecipe;
+
+        // see if the last recipe we used still works
+        if (checkPreviousRecipe()) {
+            currentRecipe = this.previousRecipe;
+            // If there is no active recipe, then we need to find one.
+        } else {
+            currentRecipe = getRecipeMap().findRecipe(Integer.MAX_VALUE, Collections.emptyList(), fluidStackList);
+        }
+        // If a recipe was found, then inputs were valid. Cache found recipe.
+        if (currentRecipe != null) {
+            this.previousRecipe = currentRecipe;
+        }
+        this.invalidInputsForRecipes = (currentRecipe == null);
+
+        // proceed if we have a usable recipe.
+        if (currentRecipe != null && checkRecipe(currentRecipe)) {
+            prepareRecipe(currentRecipe);
+        }
+    }
+
+    @Override
+    protected boolean prepareRecipe(Recipe recipe) {
+        FluidStack recipeFluidStack = recipe.getFluidInputs().get(1).getInputFluidStack();
+        FluidStack inputFluidStack = getInputFluidStack(recipeFluidStack);
+        IHeatExchanger mte = (IHeatExchanger) metaTileEntity;
+        int rMaxInletFlow = recipe.getProperty(MaxRateProperty.getInstance(), 0);
+
+        int tMaxInletFlow = (int) Math.ceil((0.4 + 0.006 * mte.getThrottle()) * ((double) currentHeat / maxHeat) * mte.getParallel() * rMaxInletFlow);
+        int threshold = (int) Math.ceil(0.01 * recipe.getProperty(FlowRateProperty.getInstance(), 0) * mte.getThrottle());
+
+        if (inputFluidStack != null) {
+            int amount = GTLiteUtils.clamp(inputFluidStack.amount, 0, tMaxInletFlow);
+            if (amount >= threshold) {
+                setSuperheat(true);
+                return setRecipe(recipe, amount, 1);
+            } else {
+                setSuperheat(false);
+                return setRecipe(recipe, amount, 0);
+            }
+        }
+        return false;
+    }
+
+    private boolean setRecipe(Recipe recipe, int amount, int i) {
+        Recipe tRecipe =  getRecipeMap().recipeBuilder().append(recipe, 1, false)
+                .clearFluidOutputs()
+                .fluidOutputs(recipe.getFluidOutputs().get(i))
+                .fluidOutputs(recipe.getFluidOutputs().get(2))
+                .build().getResult();
+        RecipeBuilder<?> recipeBuilder = getRecipeMap().recipeBuilder();
+        recipeBuilder.append(tRecipe, amount, false);
+        applyParallelBonus(recipeBuilder);
+        recipe = recipeBuilder.build().getResult();
+        if (recipe != null && setupAndConsumeRecipeInputs(recipe, getInputInventory())) {
+            setupRecipe(recipe);
+            setRate(amount);
+            return true;
+        } else {
+            metaTileEntity.doExplosion(6);
+            return false;
+        }
+    }
+
+    public FluidStack getInputFluidStack(FluidStack fluidStack) {
+        return getInputTank().drain(new FluidStack(fluidStack.getFluid(), Integer.MAX_VALUE), false);
+    }
+
+    private void setHeat(int heat) {
+        if (heat != this.currentHeat && !metaTileEntity.getWorld().isRemote) {
+            writeCustomData(GTLiteDataCode.ChannelHeatExchangerHeat, b -> b.writeVarInt(heat));
+        }
+        this.currentHeat = heat;
+    }
+
+    private void setSuperheat(boolean isSuperheat) {
+        if (this.isSuperheat != isSuperheat && !metaTileEntity.getWorld().isRemote) {
+            writeCustomData(GTLiteDataCode.ChannelHeatExchangerSuperheat, b -> b.writeBoolean(isSuperheat));
+        }
+        this.isSuperheat = isSuperheat;
+    }
+
+    private void setRate(int rate) {
+        if (this.rate != rate && ! metaTileEntity.getWorld().isRemote) {
+            writeCustomData(GTLiteDataCode.ChannelHeatExchangerRate, b -> b.writeVarInt(rate));
+        }
+        this.rate = rate;
+    }
+
+    public double getHeatEfficiency() {
+        return (double) currentHeat / maxHeat;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public boolean isSuperheat() {
+        return isSuperheat;
+    }
+
+    @Nonnull
+    @Override
+    public NBTTagCompound serializeNBT() {
+        NBTTagCompound compound = super.serializeNBT();
+        compound.setInteger("Heat", currentHeat);
+        compound.setInteger("Rate", rate);
+        compound.setBoolean("isSuperheat", isSuperheat);
+        return compound;
+    }
+
+    @Override
+    public void deserializeNBT(@Nonnull NBTTagCompound compound) {
+        super.deserializeNBT(compound);
+        currentHeat = compound.getInteger("Heat");
+        rate = compound.getInteger("Rate");
+        isSuperheat = compound.getBoolean("isSuperheat");
+    }
+
+    @Deprecated
+    @Override
+    public void writeInitialData(@Nonnull PacketBuffer buf) {
+        super.writeInitialData(buf);
+        buf.writeVarInt(currentHeat);
+        buf.writeVarInt(rate);
+        buf.writeBoolean(isSuperheat);
+    }
+
+    @Deprecated
+    @Override
+    public void receiveInitialData(@Nonnull PacketBuffer buf) {
+        super.receiveInitialData(buf);
+        this.currentHeat = buf.readVarInt();
+        this.rate = buf.readVarInt();
+        this.isSuperheat = buf.readBoolean();
+    }
+
+    @Override
+    public void receiveCustomData(int dataId, @Nonnull PacketBuffer buf) {
+        super.receiveCustomData(dataId, buf);
+        if (dataId == GTLiteDataCode.ChannelHeatExchangerHeat) {
+            this.currentHeat = buf.readVarInt();
+        }
+        if (dataId == GTLiteDataCode.ChannelHeatExchangerSuperheat) {
+            this.isSuperheat = buf.readBoolean();
+        }
+        if (dataId == GTLiteDataCode.ChannelHeatExchangerRate) {
+            this.rate = buf.readVarInt();
+        }
+    }
+}
---
+++ src/main/java/magicbook/gtlitecore/common/metatileentities/multi/electric/adv/MetaTileEntityMegaHeatExchanger.java
@@ -0,0 +1,209 @@
+package magicbook.gtlitecore.common.metatileentities.multi.electric.adv;
+
+import gregtech.api.gui.GuiTextures;
+import gregtech.api.gui.Widget;
+import gregtech.api.gui.widgets.ClickButtonWidget;
+import gregtech.api.gui.widgets.WidgetGroup;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.utils.TooltipHelper;
+import gregtech.common.blocks.MetaBlocks;
+import magicbook.gtlitecore.api.capability.impl.HeatExchangerLogic;
+import magicbook.gtlitecore.api.metatileentity.multi.IHeatExchanger;
+import magicbook.gtlitecore.api.metatileentity.multi.NoEnergyMultiblockController;
+import magicbook.gtlitecore.api.recipe.GTLiteRecipeMaps;
+import magicbook.gtlitecore.api.unification.GTLiteMaterials;
+import magicbook.gtlitecore.client.GTLiteTextures;
+import magicbook.gtlitecore.common.blocks.BlockBoilerCasing;
+import magicbook.gtlitecore.common.blocks.BlockMetalCasing;
+import magicbook.gtlitecore.common.blocks.GTLiteMetaBlocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+import static gregtech.api.gui.widgets.AdvancedTextWidget.withHoverTextTranslate;
+import static net.minecraft.util.text.TextFormatting.*;
+
+public class MetaTileEntityMegaHeatExchanger extends NoEnergyMultiblockController implements IHeatExchanger {
+
+    private final int parallel = 64;
+    private final int heatTime = 150 * 16;
+    private int thresholdPercentage = 100;
+
+    public MetaTileEntityMegaHeatExchanger(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, GTLiteRecipeMaps.HEAT_EXCHANGE_RECIPES);
+        this.recipeMapWorkable = new HeatExchangerLogic(this);
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityMegaHeatExchanger(metaTileEntityId);
+    }
+
+    @Nonnull
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start()
+                .aisle(" F F ", " F F ", " CCC ", " CCC ", " CCC ", " CCC ", " CCC ", " CCC ", " CCC ")
+                .aisle("F   F", "F   F", "CCCCC", "CPPPC", "CPPPC", "CPPPC", "CPPPC", "CPPPC", "CCCCC")
+                .aisle("     ", "     ", "CCICC", "CPPPC", "CPPPC", "CPPPC", "CPPPC", "CPPPC", "CCECC")
+                .aisle("F   F", "F   F", "CCCCC", "CPPPC", "CPPPC", "CPPPC", "CPPPC", "CPPPC", "CCCCC")
+                .aisle(" F F ", " F F ", " CCC ", " CSC ", " CCC ", " CCC ", " CCC ", " CCC ", " CCC ")
+                .where('S', selfPredicate())
+                .where('C', states(getCasingState())
+                        .setMinGlobalLimited(88)
+                        .or(abilities(MultiblockAbility.EXPORT_FLUIDS))
+                        .or(abilities(MultiblockAbility.IMPORT_FLUIDS)))
+                .where('P', states(getBoilerCasingState()))
+                .where('I', abilities(MultiblockAbility.IMPORT_FLUIDS))
+                .where('E', abilities(MultiblockAbility.EXPORT_FLUIDS))
+                .where('F', states(getFrameState()))
+                .where(' ', any())
+                .build();
+    }
+
+    private static IBlockState getCasingState() {
+        return GTLiteMetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.AUSTENITIC_STAINLESS_STEEL_CASING);
+    }
+
+    private static IBlockState getBoilerCasingState() {
+        return GTLiteMetaBlocks.BOILER_CASING.getState(BlockBoilerCasing.BoilerCasingType.POLYBENZIMIDAZOLE);
+    }
+
+    private static IBlockState getFrameState() {
+        return MetaBlocks.FRAMES.get(GTLiteMaterials.AusteniticStainlessSteel904L).getBlock(GTLiteMaterials.AusteniticStainlessSteel904L);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart iMultiblockPart) {
+        return GTLiteTextures.AUSTENITIC_STAINLESS_STEEL_CASING;
+    }
+
+    @Override
+    protected void addDisplayText(List<ITextComponent> textList) {
+        super.addDisplayText(textList);
+        if (isStructureFormed()) {
+            HeatExchangerLogic logic = (HeatExchangerLogic)recipeMapWorkable;
+            textList.add(new TextComponentTranslation("gtlitecore.machine.heat_exchanger.rate." + logic.isSuperheat(), logic.getRate()));
+            int efficiency = (int) Math.ceil(((HeatExchangerLogic)recipeMapWorkable).getHeatEfficiency() * (40 + 0.6 * thresholdPercentage));
+            textList.add(new TextComponentTranslation("gtlitecore.machine.heat_exchanger.efficiency",
+                    (efficiency == 0 ? DARK_RED : efficiency <= 40 ? RED : efficiency == 100 ? GREEN : YELLOW).toString() + efficiency + "%"));
+            ITextComponent throttleText = new TextComponentTranslation("gtlitecore.machine.heat_exchanger.threshold",
+                    AQUA.toString() + getThrottle() + "%");
+            withHoverTextTranslate(throttleText, "gtlitecore.machine.heat_exchanger.threshold.tooltip");
+            textList.add(throttleText);
+        }
+    }
+
+    @Override
+    @Nonnull
+    protected Widget getFlexButton(int x, int y, int width, int height) {
+        WidgetGroup group = new WidgetGroup(x, y, width, height);
+        group.addWidget(new ClickButtonWidget(0, 0, 9, 18, "", this::decrementThreshold)
+                .setButtonTexture(GuiTextures.BUTTON_THROTTLE_MINUS)
+                .setTooltipText("gtlitecore.machine.heat_exchanger.threshold_decrement"));
+        group.addWidget(new ClickButtonWidget(9, 0, 9, 18, "", this::incrementThreshold)
+                .setButtonTexture(GuiTextures.BUTTON_THROTTLE_PLUS)
+                .setTooltipText("gtlitecore.machine.heat_exchanger.threshold_increment"));
+        return group;
+    }
+
+    private void incrementThreshold(Widget.ClickData clickData) {
+        this.thresholdPercentage = MathHelper.clamp(thresholdPercentage + 5, 25, 100);
+    }
+
+    private void decrementThreshold(Widget.ClickData clickData) {
+        this.thresholdPercentage = MathHelper.clamp(thresholdPercentage - 5, 25, 100);
+    }
+
+    @Override
+    public String[] getDescription() {
+        return new String[]{I18n.format("gtlitecore.machine.heat_exchanger.description")};
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, @Nonnull List<String> tooltip, boolean advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
+        tooltip.add(I18n.format("gtlitecore.machine.heat_exchanger.heat_time_tooltip", heatTime));
+        tooltip.add(I18n.format("gtlitecore.machine.heat_exchanger.tooltip.parallel", parallel));
+        tooltip.add(TooltipHelper.BLINKING_RED + I18n.format("gtlitecore.machine.heat_exchanger.explosion_tooltip"));
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        data.setInteger("ThresholdPercentage", thresholdPercentage);
+        return super.writeToNBT(data);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        thresholdPercentage = data.getInteger("ThresholdPercentage");
+        super.readFromNBT(data);
+    }
+
+    @Override
+    public void writeInitialSyncData(PacketBuffer buf) {
+        super.writeInitialSyncData(buf);
+        buf.writeVarInt(thresholdPercentage);
+    }
+
+    @Override
+    public void receiveInitialSyncData(PacketBuffer buf) {
+        super.receiveInitialSyncData(buf);
+        thresholdPercentage = buf.readVarInt();
+    }
+
+    @Override
+    public int getThrottle() {
+        return thresholdPercentage;
+    }
+
+    @Override
+    public int getHeatTime() {
+        return heatTime;
+    }
+
+    @Override
+    public int getParallel() {
+        return parallel;
+    }
+
+    @Override
+    public boolean hasMaintenanceMechanics() {
+        return false;
+    }
+
+    @Override
+    public boolean canVoidRecipeItemOutputs() {
+        return true;
+    }
+
+    @Override
+    public boolean canVoidRecipeFluidOutputs() {
+        return true;
+    }
+
+    @Override
+    protected boolean shouldShowVoidingModeButton() {
+        return false;
+    }
+}
