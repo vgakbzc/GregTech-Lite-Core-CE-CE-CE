f40369cb3c7c1feff401bd6fe735bf09f40a9c48
Magic_Sweepy <admin>
2024-01-11 11:19:31 +0800

    Add more configs.

--- src/main/java/magicbook/gtlitecore/loaders/circuits/QuantumCircuits.java
+++ src/main/java/magicbook/gtlitecore/loaders/circuits/QuantumCircuits.java
@@ -3,6 +3,7 @@ package magicbook.gtlitecore.loaders.circuits;
 import gregtech.api.metatileentity.multiblock.CleanroomType;
 import gregtech.api.recipes.GTRecipeHandler;
 import gregtech.api.unification.OreDictUnifier;
+import magicbook.gtlitecore.common.GTLiteConfigHolder;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -18,69 +19,72 @@ public class QuantumCircuits {
     }
 
     private static void AssemblyBuffer() {
-        //  Delete original recipe
-        GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
-                new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
-                        QUANTUM_PROCESSOR_EV.getStackForm(2),
-                        SMD_INDUCTOR.getStackForm(8),
-                        SMD_CAPACITOR.getStackForm(16),
-                        RANDOM_ACCESS_MEMORY.getStackForm(4),
-                        OreDictUnifier.get(wireFine, Platinum, 16)},
-                new FluidStack[]{Tin.getFluid(288)});
 
-        GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
-                new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
-                        QUANTUM_PROCESSOR_EV.getStackForm(2),
-                        SMD_INDUCTOR.getStackForm(8),
-                        SMD_CAPACITOR.getStackForm(16),
-                        RANDOM_ACCESS_MEMORY.getStackForm(4),
-                        OreDictUnifier.get(wireFine, Platinum, 16)},
-                new FluidStack[]{SolderingAlloy.getFluid(144)});
+        if (GTLiteConfigHolder.gtlitecoreOverrides.buffProcessorAssemblyRecipe) {
+            //  Delete original recipe
+            GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
+                    new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
+                            QUANTUM_PROCESSOR_EV.getStackForm(2),
+                            SMD_INDUCTOR.getStackForm(8),
+                            SMD_CAPACITOR.getStackForm(16),
+                            RANDOM_ACCESS_MEMORY.getStackForm(4),
+                            OreDictUnifier.get(wireFine, Platinum, 16)},
+                    new FluidStack[]{Tin.getFluid(288)});
 
-        GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
-                new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
-                        QUANTUM_PROCESSOR_EV.getStackForm(2),
-                        ADVANCED_SMD_INDUCTOR.getStackForm(2),
-                        ADVANCED_SMD_CAPACITOR.getStackForm(4),
-                        RANDOM_ACCESS_MEMORY.getStackForm(4),
-                        OreDictUnifier.get(wireFine, Platinum, 16)},
-                new FluidStack[]{Tin.getFluid(288)});
+            GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
+                    new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
+                            QUANTUM_PROCESSOR_EV.getStackForm(2),
+                            SMD_INDUCTOR.getStackForm(8),
+                            SMD_CAPACITOR.getStackForm(16),
+                            RANDOM_ACCESS_MEMORY.getStackForm(4),
+                            OreDictUnifier.get(wireFine, Platinum, 16)},
+                    new FluidStack[]{SolderingAlloy.getFluid(144)});
 
-        GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
-                new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
-                        QUANTUM_PROCESSOR_EV.getStackForm(2),
-                        ADVANCED_SMD_INDUCTOR.getStackForm(2),
-                        ADVANCED_SMD_CAPACITOR.getStackForm(4),
-                        RANDOM_ACCESS_MEMORY.getStackForm(4),
-                        OreDictUnifier.get(wireFine, Platinum, 16)},
-                new FluidStack[]{SolderingAlloy.getFluid(144)});
+            GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
+                    new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
+                            QUANTUM_PROCESSOR_EV.getStackForm(2),
+                            ADVANCED_SMD_INDUCTOR.getStackForm(2),
+                            ADVANCED_SMD_CAPACITOR.getStackForm(4),
+                            RANDOM_ACCESS_MEMORY.getStackForm(4),
+                            OreDictUnifier.get(wireFine, Platinum, 16)},
+                    new FluidStack[]{Tin.getFluid(288)});
 
-        CIRCUIT_ASSEMBLER_RECIPES.recipeBuilder()
-                .input(EXTREME_CIRCUIT_BOARD)
-                .input(QUANTUM_PROCESSOR_EV, 2)
-                .input(SMD_INDUCTOR, 8)
-                .input(SMD_CAPACITOR, 16)
-                .input(RANDOM_ACCESS_MEMORY, 16)
-                .input(wireFine, Platinum, 16)
-                .solderMultiplier(2)
-                .output(QUANTUM_ASSEMBLY_IV, 2)
-                .EUt(2400)
-                .duration(400)
-                .cleanroom(CleanroomType.CLEANROOM)
-                .buildAndRegister();
+            GTRecipeHandler.removeRecipesByInputs(CIRCUIT_ASSEMBLER_RECIPES,
+                    new ItemStack[]{EXTREME_CIRCUIT_BOARD.getStackForm(),
+                            QUANTUM_PROCESSOR_EV.getStackForm(2),
+                            ADVANCED_SMD_INDUCTOR.getStackForm(2),
+                            ADVANCED_SMD_CAPACITOR.getStackForm(4),
+                            RANDOM_ACCESS_MEMORY.getStackForm(4),
+                            OreDictUnifier.get(wireFine, Platinum, 16)},
+                    new FluidStack[]{SolderingAlloy.getFluid(144)});
 
-        CIRCUIT_ASSEMBLER_RECIPES.recipeBuilder()
-                .input(EXTREME_CIRCUIT_BOARD)
-                .input(QUANTUM_PROCESSOR_EV, 2)
-                .input(ADVANCED_SMD_INDUCTOR, 2)
-                .input(ADVANCED_SMD_CAPACITOR, 4)
-                .input(RANDOM_ACCESS_MEMORY, 16)
-                .input(wireFine, Platinum, 16)
-                .solderMultiplier(2)
-                .output(QUANTUM_ASSEMBLY_IV, 2)
-                .EUt(2400)
-                .duration(200)
-                .cleanroom(CleanroomType.CLEANROOM)
-                .buildAndRegister();
+            CIRCUIT_ASSEMBLER_RECIPES.recipeBuilder()
+                    .input(EXTREME_CIRCUIT_BOARD)
+                    .input(QUANTUM_PROCESSOR_EV, 2)
+                    .input(SMD_INDUCTOR, 8)
+                    .input(SMD_CAPACITOR, 16)
+                    .input(RANDOM_ACCESS_MEMORY, 16)
+                    .input(wireFine, Platinum, 16)
+                    .solderMultiplier(2)
+                    .output(QUANTUM_ASSEMBLY_IV, 2)
+                    .EUt(2400)
+                    .duration(400)
+                    .cleanroom(CleanroomType.CLEANROOM)
+                    .buildAndRegister();
+
+            CIRCUIT_ASSEMBLER_RECIPES.recipeBuilder()
+                    .input(EXTREME_CIRCUIT_BOARD)
+                    .input(QUANTUM_PROCESSOR_EV, 2)
+                    .input(ADVANCED_SMD_INDUCTOR, 2)
+                    .input(ADVANCED_SMD_CAPACITOR, 4)
+                    .input(RANDOM_ACCESS_MEMORY, 16)
+                    .input(wireFine, Platinum, 16)
+                    .solderMultiplier(2)
+                    .output(QUANTUM_ASSEMBLY_IV, 2)
+                    .EUt(2400)
+                    .duration(200)
+                    .cleanroom(CleanroomType.CLEANROOM)
+                    .buildAndRegister();
+        }
     }
 }
