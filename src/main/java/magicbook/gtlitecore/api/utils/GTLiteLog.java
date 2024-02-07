af2db12e8b076188794edb5dc1d5d088c3b5d87a
Magic_Sweepy <admin>
2024-02-07 16:47:56 +0800

    Clean up.

--- src/main/java/magicbook/gtlitecore/api/utils/GTLiteLog.java
+++ src/main/java/magicbook/gtlitecore/api/utils/GTLiteLog.java
@@ -1,14 +1,11 @@
 package magicbook.gtlitecore.api.utils;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class GTLiteLog {
 
-    public static Logger logger;
+    public static Logger logger = LogManager.getLogger("GregTech Lite Core");
 
     public GTLiteLog() {}
-
-    public static void init(Logger modLogger) {
-        logger = modLogger;
-    }
 }
