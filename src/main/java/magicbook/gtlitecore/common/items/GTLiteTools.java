92a1b4dac662c088c8a312eb3c09c2d7e2cde1c0
Magic_Sweepy <admin>
2024-02-24 14:08:12 +0800

    Add Vajra.

--- src/main/java/magicbook/gtlitecore/common/items/GTLiteTools.java
+++ src/main/java/magicbook/gtlitecore/common/items/GTLiteTools.java
@@ -3,12 +3,10 @@ package magicbook.gtlitecore.common.items;
 import gregtech.api.GTValues;
 import gregtech.api.items.toolitem.*;
 import gregtech.common.items.ToolItems;
-import gregtech.common.items.tool.BlockRotatingBehavior;
-import gregtech.common.items.tool.EntityDamageBehavior;
-import gregtech.common.items.tool.GrassPathBehavior;
-import gregtech.common.items.tool.RotateRailBehavior;
+import gregtech.common.items.tool.*;
 import gregtech.core.sound.GTSoundEvents;
 import magicbook.gtlitecore.GTLiteCore;
+import magicbook.gtlitecore.api.GTLiteValues;
 import net.minecraft.entity.monster.EntityGolem;
 import net.minecraft.init.SoundEvents;
 
@@ -19,14 +17,14 @@ public class GTLiteTools {
     public static IGTTool ELECTRIC_JACKHAMMER_LV;
     public static IGTTool ELECTRIC_JACKHAMMER_HV;
     public static IGTTool ELECTRIC_JACKHAMMER_IV;
+    public static IGTTool VAJRA;
 
     private GTLiteTools() {}
 
     public static void init() {
 
         COMBINATION_WRENCH = ToolItems.register(
-                ItemGTTool.Builder
-                        .of(GTLiteCore.MODID, "combination_wrench")
+                ItemGTTool.Builder.of(GTLiteValues.MODID, "combination_wrench")
                         .toolStats(b -> b
                                 .blockBreaking()
                                 .crafting()
@@ -41,8 +39,7 @@ public class GTLiteTools {
         );
 
         UNIVERSAL_SPADE = ToolItems.register(
-                ItemGTTool.Builder
-                        .of(GTLiteCore.MODID, "universal_spade")
+                ItemGTTool.Builder.of(GTLiteValues.MODID, "universal_spade")
                         .toolStats(b -> b
                                 .blockBreaking()
                                 .crafting()
@@ -57,8 +54,7 @@ public class GTLiteTools {
         );
 
         ELECTRIC_JACKHAMMER_LV = ToolItems.register(
-                ItemGTTool.Builder
-                        .of(GTLiteCore.MODID, "electric_jackhammer.lv")
+                ItemGTTool.Builder.of(GTLiteValues.MODID, "electric_jackhammer.lv")
                         .toolStats(b -> b
                                 .blockBreaking()
                                 .attacking()
@@ -76,8 +72,7 @@ public class GTLiteTools {
         );
 
         ELECTRIC_JACKHAMMER_HV = ToolItems.register(
-                ItemGTTool.Builder
-                        .of(GTLiteCore.MODID, "electric_jackhammer.hv")
+                ItemGTTool.Builder.of(GTLiteValues.MODID, "electric_jackhammer.hv")
                         .toolStats(b -> b
                                 .blockBreaking()
                                 .attacking()
@@ -95,8 +90,7 @@ public class GTLiteTools {
         );
 
         ELECTRIC_JACKHAMMER_IV = ToolItems.register(
-                ItemGTTool.Builder
-                        .of(GTLiteCore.MODID, "electric_jackhammer.iv")
+                ItemGTTool.Builder.of(GTLiteValues.MODID, "electric_jackhammer.iv")
                         .toolStats(b -> b
                                 .blockBreaking()
                                 .attacking()
@@ -111,6 +105,22 @@ public class GTLiteTools {
                         .sound(SoundEvents.BLOCK_ANVIL_LAND, true)
                         .toolClasses("pickaxe", "hammer")
                         .electric(GTValues.IV)
+        );
+
+        VAJRA = ToolItems.register(
+                ItemGTTool.Builder.of(GTLiteValues.MODID, "vajra")
+                        .toolStats(b -> b
+                                .blockBreaking()
+                                .attacking()
+                                .damagePerAction(2)
+                                .attackDamage(10.0F)
+                                .attackSpeed(22.4F)
+                                .efficiencyMultiplier(10F)
+                                .brokenStack(ToolHelper.SUPPLY_POWER_UNIT_EV)
+                                .behaviors(TreeFellingBehavior.INSTANCE))
+                        .sound(GTSoundEvents.ASSEMBLER, true)
+                        .toolClasses("pickaxe", "axe", "sword", "shovel", "wrench", "wirecutter")
+                        .electric(GTValues.EV)
         );
 
     }
