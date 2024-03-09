a640f97d069080ea7d5ce6dac64ea63a49dcb2e4
Magic_Sweepy <admin>
2024-03-09 10:06:17 +0800

    Another Clean.

--- src/main/java/magicbook/gtlitecore/api/utils/ServerSupportI18n.java
+++ src/main/java/magicbook/gtlitecore/api/utils/ServerSupportI18n.java
@@ -16,17 +16,20 @@ import net.minecraftforge.fml.relauncher.Side;
 public class ServerSupportI18n {
 
     /**
-     * @param clientName Client side localized name.
-     * @param serverName Server name (use english).
-     * @return Same as I18n.format(), but this method is server supported. Please use it for Recipe Property init (in Common Proxy).
+     * Format with Client and Server two names.
+     *
+     * @param clientName  Client side localized name.
+     * @param serverName  Server name (use english).
+     * @return  Same as I18n.format(), but this method is server supported. Please use it for Recipe Property init (in Common Proxy).
      */
     public static String format(String clientName, String serverName) {
         return FMLLaunchHandler.side() == Side.CLIENT ? clientHelper(clientName) : serverName;
     }
 
     /**
-     * @param clientName Client side localized name, i.e. I18n.format().
-     * @return Same as  I18n.format().
+     * Client name helper.
+     * @param clientName  Client side localized name, i.e. I18n.format().
+     * @return  Same as  I18n.format().
      */
     public static String clientHelper(String clientName) {
         return I18n.format(clientName);
