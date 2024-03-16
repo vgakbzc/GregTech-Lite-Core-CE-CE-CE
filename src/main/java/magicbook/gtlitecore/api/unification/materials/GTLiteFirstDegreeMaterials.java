0bc6e36914ba0fb3799b836effdc96e6d84f8354
Magic_Sweepy <admin>
2024-03-16 22:51:24 +0800

    Fix some chemistry problems.

--- src/main/java/magicbook/gtlitecore/api/unification/materials/GTLiteFirstDegreeMaterials.java
+++ src/main/java/magicbook/gtlitecore/api/unification/materials/GTLiteFirstDegreeMaterials.java
@@ -1701,7 +1701,7 @@ public class GTLiteFirstDegreeMaterials {
                 .dust()
                 .color(0x787449)
                 .iconSet(DULL)
-                .components(Sodium, 1, Oxygen, 2, Boron, 1, Fluorine, 4)
+                .components(Nitrogen, 1, Oxygen, 2, Boron, 1, Fluorine, 4)
                 .flags(DISABLE_DECOMPOSITION)
                 .build();
 
@@ -1710,7 +1710,7 @@ public class GTLiteFirstDegreeMaterials {
                 .dust()
                 .color(0xA32A8C)
                 .iconSet(ROUGH)
-                .components(Sodium, 1, Oxygen, 1, Boron, 1, Fluorine, 4)
+                .components(Nitrogen, 1, Oxygen, 1, Boron, 1, Fluorine, 4)
                 .flags(DISABLE_DECOMPOSITION)
                 .build();
 
