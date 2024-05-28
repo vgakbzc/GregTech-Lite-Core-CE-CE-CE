bf3928dd1cd894db4f15f1b385490eaef100971e
Magic_Sweepy <admin>
2024-05-28 16:56:28 +0800

    Little fix of Teflon.

--- src/main/java/magicbook/gtlitecore/loaders/chains/TeflonChain.java
+++ src/main/java/magicbook/gtlitecore/loaders/chains/TeflonChain.java
@@ -3,10 +3,10 @@ package magicbook.gtlitecore.loaders.chains;
 import gregtech.api.metatileentity.multiblock.CleanroomType;
 
 import static gregtech.api.GTValues.*;
-import static gregtech.api.recipes.RecipeMaps.VACUUM_RECIPES;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.ore.OrePrefix.dust;
 import static magicbook.gtlitecore.api.GTLiteValues.SECOND;
+import static magicbook.gtlitecore.api.recipe.GTLiteRecipeMaps.VACUUM_CHAMBER_RECIPES;
 import static magicbook.gtlitecore.api.unification.GTLiteMaterials.Teflon;
 
 public class TeflonChain {
@@ -14,7 +14,7 @@ public class TeflonChain {
     public static void init() {
 
         //  15C2F4 + 3C2H2 + C + Na -> (C2F4)15(C2H2)3CNa
-        VACUUM_RECIPES.recipeBuilder()
+        VACUUM_CHAMBER_RECIPES.recipeBuilder()
                 .input(dust, Polytetrafluoroethylene, 15)
                 .input(dust, Polyethylene, 3)
                 .input(dust, Carbon)
