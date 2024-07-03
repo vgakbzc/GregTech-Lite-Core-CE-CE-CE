c81aa5743022e0eeb410f28e83f2a3a2a25bbd08
Magic_Sweepy <admin>
2024-07-03 17:51:16 +0800

    Fix Dynamo Hatch problem of Mega Turbines.

--- src/main/java/magicbook/gtlitecore/common/metatileentities/multi/electric/generator/MetaTileEntityMegaTurbine.java
+++ src/main/java/magicbook/gtlitecore/common/metatileentities/multi/electric/generator/MetaTileEntityMegaTurbine.java
@@ -108,7 +108,7 @@ public class MetaTileEntityMegaTurbine extends FuelMultiblockController implemen
         this.inputFluidInventory = new FluidTankList(this.allowSameFluidFillForOutputs(), this.getAbilities(MultiblockAbility.IMPORT_FLUIDS));
         this.outputInventory = new ItemHandlerList(this.getAbilities(MultiblockAbility.EXPORT_ITEMS));
         this.outputFluidInventory = new FluidTankList(this.allowSameFluidFillForOutputs(), this.getAbilities(MultiblockAbility.EXPORT_FLUIDS));
-        List<IEnergyContainer> energyContainer = new ArrayList<>(this.getAbilities(MultiblockAbility.OUTPUT_LASER));
+        List<IEnergyContainer> energyContainer = new ArrayList<>(this.getAbilities(MultiblockAbility.OUTPUT_ENERGY));
         energyContainer.addAll(this.getAbilities(MultiblockAbility.OUTPUT_LASER));
         this.energyContainer = new EnergyContainerList(energyContainer);
     }
